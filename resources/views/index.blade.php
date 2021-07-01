8ef20687578596fc6eec674276d584e2ee529644
Clayton Rafael <clayton.rssouza@gmail.com>
2021-07-01 09:57:55 -0300

    Instalação e configuração do Laravel + Boostrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TodoDev</title>
+    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+</head>
+
+<body class="antialiased">
+    <h1>TodoDev</h1>
+
+
+    <!-- Scripts -->
+</body>
+
+</html>
